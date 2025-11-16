e4d9be3a3ecb5acfbe3ae7fb940ed8b7b946d41b
sseen2 <kse10023@naver.com>
2025-11-16 15:19:39 +0900

    chore: 웹소켓 의존성 및 설정 파일 추가

---
+++ src/main/java/racingcar/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package racingcar.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private static final String WEBSOCKET_PATH = "/car-ws";
+
+    private static final String WEBSOCKET_SUBSCRIPTION_PATH = "/sub";
+    private static final String WEBSOCKET_PUBLISH_PATH = "/pub";
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint(WEBSOCKET_PATH)
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry messageBrokerRegistry) {
+        messageBrokerRegistry.enableSimpleBroker(WEBSOCKET_SUBSCRIPTION_PATH);
+        messageBrokerRegistry.setApplicationDestinationPrefixes(WEBSOCKET_PUBLISH_PATH);
+    }
+}
